bb622e66b51038d4b7c42da75626807905fdbd38
wodpachua <molarag21996@gmail.com>
2024-07-07 12:18:58 +0300

    Test First TDD Cycle 2

--- test/test.js
+++ test/test.js
@@ -5,6 +5,11 @@ describe("Finding number in Fibonacci Series", function () {
   it("4th number in series should be '3'", function () {
     assert.equal(fibonacci(4), 3);
   });
+
+  it("1st and 2nd number in series should be '1'", function () {
+    assert.equal(fibonacci(1), 1);
+    assert.equal(fibonacci(2), 1);
+  });
 });
 
 // PAIR PARTNER
